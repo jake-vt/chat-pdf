e6c5a603c0700d086280aa244f07ee31570604a7
Jacob Thompson <jakethompson77@hotmail.co.uk>
2023-06-16 22:00:47 +0100

    adds user question box, LLM response

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from dotenv import load_dotenv
 import streamlit as st
 from pypdf import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
 
 def main():
 	load_dotenv()
@@ -27,8 +31,22 @@ def main():
 		)
 		chunks = text_splitter.split_text(text)
 
-		st.write(chunks)
+		# create embeddings
+		embeddings = OpenAIEmbeddings()
+		knowledge_base = FAISS.from_texts(chunks, embeddings)
 
+
+		# show user input
+		user_question = st.text_input("Ask a question about your PDF:")
+		if user_question:
+			docs = knowledge_base.similarity_search(user_question)
+
+            # query LLM
+			llm = OpenAI()
+			chain = load_qa_chain(llm, chain_type="stuff")
+			response = chain.run(input_documents=docs, question=user_question)
+
+			st.write(response)
 
 if __name__ == "__main__":
 	main()
